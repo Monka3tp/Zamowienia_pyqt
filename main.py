cf75840502e8f29453b70e4a8339b7444214d3f7
Wilkolak3 <patrykjanusz@onet.pl>
2024-10-17 09:28:22 +0200

    kod 17.10 radiobuttony rozmiar i platnosc

--- main.py
+++ main.py
@@ -10,8 +10,36 @@ class MyForm(QDialog):
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+        self.ui.sButton.toggled.connect(self.size)
+        self.ui.mButton.toggled.connect(self.size)
+        self.ui.lButton.toggled.connect(self.size)
+        self.ui.xlButton.toggled.connect(self.size)
+        self.ui.kartaButton.toggled.connect(self.size)
+        self.ui.blikButton.toggled.connect(self.size)
+        self.ui.odbiorButton.toggled.connect(self.size)
         self.show()
 
+    def size(self):
+        rozmiar = 'Wybierz'
+        platnosc = 'Wybierz'
+        if self.ui.sButton.isChecked():
+            rozmiar = 'S'
+        if self.ui.mButton.isChecked():
+            rozmiar = 'M'
+        if self.ui.lButton.isChecked():
+            rozmiar = 'L'
+        if self.ui.xlButton.isChecked():
+            rozmiar = 'XL'
+
+
+        if self.ui.kartaButton.isChecked():
+            platnosc = 'karta płatnicza'
+        if self.ui.blikButton.isChecked():
+            platnosc = 'blik'
+        if self.ui.odbiorButton.isChecked():
+            platnosc = 'płatność przy odbiorze'
+
+        self.ui.outputLabel.setText(f'Wybrano rozmiar: {rozmiar}, Metoda płatności: {platnosc}')
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MyForm()
